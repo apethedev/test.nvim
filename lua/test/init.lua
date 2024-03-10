f90ba94362f891b7753e45b2b6dcd58abbc9c6c8
kelvin@fuji <kelvin@kmatrix.com>
2024-03-10 18:10:17 +0800

    Enhanced module

--- lua/test/init.lua
+++ lua/test/init.lua
@@ -1,7 +1,15 @@
 local M = {}
 
-function M.setup()
-  print("Hello HK")
+function M.setup(opts)
+  opts = opts or {}
+
+  vim.keymap.set("n", "Leader>h", function()
+    if opts.name then
+      print("Hello, " .. opts.name)
+    else
+      print("Hello")
+    end
+  end)
 end
 
 return M
